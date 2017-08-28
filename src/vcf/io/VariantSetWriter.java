790a4a8174143deb3f4442350c5eba7f01dd225a
Pascual Lorente <pasculorente@gmail.com>
2017-08-28 22:23:09 +0100

    Updated Copyright

--- src/vcf/io/VariantSetWriter.java
+++ src/vcf/io/VariantSetWriter.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) UICHUIMI 2017
+ *
+ * This file is part of VariantCallFormat.
+ *
+ * VariantCallFormat is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * VariantCallFormat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with VariantCallFormat.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package vcf.io;
 
 import vcf.Variant;
@@ -63,6 +87,7 @@ public class VariantSetWriter implements AutoCloseable {
 		if (!headerWritten) {
 			writer.write(vcfHeader.toString());
 			writer.newLine();
+			writer.flush();
 			headerWritten = true;
 		}
 	}
@@ -76,11 +101,12 @@ public class VariantSetWriter implements AutoCloseable {
 	 *                     an I/O error occurs
 	 */
 	public void write(Variant variant) throws IOException, VariantException {
-		writeHeader();
 		if (variant.getVcfHeader() != vcfHeader)
 			throw new VariantException("Variant VcfHeader does not correspond to file VcfHeader");
+		writeHeader();
 		writer.write(variant.toString());
 		writer.newLine();
+		writer.flush();
 	}
 
 	@Override
